a70c208fdecb8394624a6b8930754b570555167f
Véricel Grégory <110909861+gregmelo@users.noreply.github.com>
2024-01-22 10:55:41 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,10 +3,6 @@ import { readFileSync, writeFileSync } from "fs";
 const test = readFileSync("test.txt", "utf8");
 const final = readFileSync("final.txt", "utf8");
 
-const displayResult = (day, result) => {
-  console.log(`After ${day} days: ${result.join(",")}`);
-};
-
 const part1 = (data) => {
   const DAYS = 80; // Changer le nombre de jours selon vos besoins
   const fishs = data.split(",").map(Number);
